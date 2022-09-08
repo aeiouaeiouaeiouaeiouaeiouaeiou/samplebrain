cd30c10893f2b684680261027593c20b969ec40e
Dave Griffiths <dave@thentrythis.org>
2022-09-08 12:21:53 +0100

    copyright updated

--- brain/src/fft.h
+++ brain/src/fft.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2015 Foam Kernow
+// Copyright (C) 2015 2022 Then Try This
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
